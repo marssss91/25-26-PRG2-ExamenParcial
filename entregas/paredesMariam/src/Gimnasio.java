10c43bb0765e2c02167da1c3ea859bbae6fe052a
Your Name <you@example.com>
2026-04-06 12:50:29 +0200

    Agregando la clase Gimnasio

--- entregas/paredesMariam/src/Gimnasio.java
+++ entregas/paredesMariam/src/Gimnasio.java
@@ -1,1 +1,31 @@
-p
+package vistas.gimnasio;
+
+public class Gimnasio 
+
+    public Gimnasio(String actividades, String monitor, String fecha, int ocupacion) 
+
+    public Gimnasio(String actividades, String monitor) 
+
+    public Gimnasio() 
+
+    public Gimnasio(Gimnasio gimnasio) 
+
+    public Gimnasio clone() 
+
+    public String monitor() 
+
+    public String actividades() 
+
+    public void hacerReservas() 
+
+    public void consultarReservas() 
+    public void cancelarReservas() 
+
+    public void inscribirSocios()
+
+    public void verReservas() 
+
+    public void verSocios(String nombre) 
+    public String toString() 
+
+    public void mostrar() 
