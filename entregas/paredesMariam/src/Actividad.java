9ba8add7097e3e559c00b4b5d473b7169bc98842
Your Name <you@example.com>
2026-04-06 12:49:04 +0200

    Agrengando clase Actividad

---
+++ entregas/paredesMariam/src/Actividad.java
@@ -0,0 +1,26 @@
+package vistas.gimnasio;
+
+public class Actividades 
+
+    private String nombre;
+    private String monitor;
+    private String horario;
+
+    
+    public Actividades(String nombre, String monitor, String horario) 
+
+    public Actividades(Actividades actividades) 
+
+    public Actividades clone() 
+
+    public String fecha() 
+
+    public boolean estaEnActividad() 
+
+    public boolean estaPendiente() 
+    public boolean equals(Actividades actividades) 
+    public void registrarActividad(String nombre, int dni) 
+
+    public String toString() 
+
+    public void mostrar() 
