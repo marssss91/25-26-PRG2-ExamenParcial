0b9e26b813d052e0513e7fb2693842296e969208
Your Name <you@example.com>
2026-04-06 12:45:59 +0200

    Agregando la clase socio

---
+++ entregas/paredesMariam/src/Socio.java
@@ -0,0 +1,34 @@
+package vistas.gimnasio;
+
+public class Socio 
+
+public Socio(String nombre, int dni, String suscripcion) 
+
+public Socio(String nombre, int dni)
+
+public Socio()
+
+public Socio(Socio socio)
+
+public Socio clone()
+
+public String nombre() 
+
+public int dni() 
+
+    public String suscripcion() 
+
+    public boolean equals(Socio socio) 
+
+    public boolean tieneSuscripcion(String nombre, int dni, String suscripcion) 
+
+    public boolean tieneMismoNombre(String nombre) 
+
+    public void agregarActividad(String actividades) 
+
+    public void reiniciarActividad() 
+
+    public void reiniciarReserva() 
+    public String toString() 
+
+    public void mostrar() 
